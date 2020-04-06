9fab1536d99fd591968d1c15192e4487d714aa1a
_Avie <lasergijs@outlook.com>
2020-04-06 22:39:35 +0200

    Added support for the feed page
    Redid the way the feed panel was select

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import * as ReactDom from "react-dom";
 
 import { ChatPanel } from "./components/ChatPanel/ChatPanel";
 import { SidePanel } from "./components/SidePanel/SidePanel";
+import { FeedPanel } from "./components/FeedPanel/FeedPanel"
 
 interface AppState
 {
-	// The id of the current chat
-	CurrentChat: Number;
+	// The id of the current chat or "Feed"
+	CurrentChat: Number | "Feed";
 }
 
 // The app
@@ -16,17 +17,27 @@ class App extends React.Component<{},AppState>
 	constructor(props: object)
 	{
 		super(props)
-		// TODO: should be the homepage
-		this.state = {CurrentChat: 554};
+		this.state = {CurrentChat: "Feed"};
 	}
 
 	render()
 	{
 		return <div>
 			<SidePanel CallBack={(id) => this.setState({CurrentChat: id})}/>
-			{/* TODO: implement feed */}
-			<ChatPanel ChatId = {this.state.CurrentChat}/>
+			{/* Decides wether it should render feed or a chat. */}
+			{this.ChoosePanel() }
 		</div>
+	}
+	/*
+	* Decides wether it should render feed or a chat.
+	* This is decided based on the currentChat in the state.
+	* If current chat is "Feed" it will render the feed else it will render the chat with the id of CurrentChat.
+	*/
+	ChoosePanel()
+	{
+		if(this.state.CurrentChat === "Feed")
+			return <FeedPanel/>
+		return <ChatPanel ChatId = {this.state.CurrentChat as number}/>
 	}
 }
 
