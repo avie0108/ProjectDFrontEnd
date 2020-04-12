37051ca7d80992af5435a03b7f06c5fbaaebe4c2
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-04-12 11:43:12 +0200

    handleForm
    zet de form data in de state

--- src/components/FeedPanel/FeedPanel.tsx
+++ src/components/FeedPanel/FeedPanel.tsx
@@ -1,38 +1,65 @@
-import React from 'react';
-import { FeedItem } from './FeedItem';
-import './FeedPanel.scss';
+import React from "react";
+import { FeedItem } from "./FeedItem";
+import "./FeedPanel.scss";
 
 // The feed panel is a container for feed items
 export class FeedPanel extends React.Component {
-    // Add feed items to the panel
-    getFeedItems() {
-        let rows = [];
+  state = { titel: "", tekstveld: "" };
 
-        // For now we add 8 feed items to the panel
-        for (let i = 0; i < 8; i++) {
-            rows.push(<FeedItem />);
-        }
+  // Add feed items to the panel
+  getFeedItems() {
+    let rows = [];
 
-        return rows;
+    // For now we add 8 feed items to the panel
+    for (let i = 0; i < 8; i++) {
+      rows.push(<FeedItem />);
     }
 
-    render() {
-        return (
-            <div className="feed-panel">
-                <div className="feed-items">
-                    <h2>Feed Items</h2>
-                    {this.getFeedItems()}
-                </div>
-                <div className="feed-form">
-                    <h2>Feed Item aanmaken</h2>
-                    <form action="/" method="POST">
-                        <input type="text" className="form-element" placeholder="Titel"></input><br/>
-                        <textarea className="form-element" placeholder="Typ uw bericht"></textarea><br/>
+    return rows;
+  }
 
-                        <input type="submit" id="feed-form-submit" value="Aanmaken"></input>
-                    </form>
-                </div>
-            </div>
-        );
+  render() {
+    return (
+      <div className="feed-panel">
+        <div className="feed-items">
+          <h2>Feed Items</h2>
+          <h3>this.state.test</h3>
+          {this.getFeedItems()}
+        </div>
+        <div className="feed-form">
+          <h2>Feed Item aanmaken</h2>
+          <form action="#">
+            <input
+              type="text"
+              className="form-element"
+              placeholder="Titel"
+              id="Titel"
+              onChange={this.handleForm}
+            ></input>
+            <br />
+            <textarea
+              className="form-element"
+              placeholder="Typ uw bericht"
+              id="Tekstveld"
+              onChange={this.handleForm}
+            ></textarea>
+            <br />
+
+            <input type="submit" id="feed-form-submit" value="Aanmaken"></input>
+          </form>
+          <span>{this.state.titel}</span>
+          <span>{this.state.tekstveld}</span>
+        </div>
+      </div>
+    );
+  }
+  handleForm = (event: any) => {
+    if (event.target.id === "Titel") {
+      this.setState({ titel: event.target.value });
     }
+    if (event.target.id === "Tekstveld") {
+      this.setState({ tekstveld: event.target.value });
+    }
+  };
+  handleFormSubmit = (event: any)
 }
