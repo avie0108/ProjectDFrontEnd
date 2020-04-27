cdd1049c879c912982cc8cbe889e416aff487afc
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-04-27 12:06:39 +0200

    popup

--- src/components/FeedPanel/FeedPanel.tsx
+++ src/components/FeedPanel/FeedPanel.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { FeedItem } from "./FeedItem";
 import "./FeedPanel.scss";
-import { sendAsJSON } from "../../ajax"
+import { sendAsJSON } from "../../ajax";
+import { PopUp } from "../Pop-up/Pop-up";
 
 // The feed panel is a container for feed items
 export class FeedPanel extends React.Component<{}, {}> {
   InputRef: React.RefObject<HTMLInputElement>;
   TextRef: React.RefObject<HTMLTextAreaElement>;
   CategorieRef: React.RefObject<HTMLInputElement>;
+  PopupRef: React.RefObject<PopUp>;
 
   constructor(props: {}) {
     super(props);
@@ -15,6 +17,7 @@ export class FeedPanel extends React.Component<{}, {}> {
     this.InputRef = React.createRef<HTMLInputElement>();
     this.TextRef = React.createRef<HTMLTextAreaElement>();
     this.CategorieRef = React.createRef<HTMLInputElement>();
+    this.PopupRef = React.createRef<PopUp>();
   }
 
   // Add feed items to the panel
@@ -33,49 +36,65 @@ export class FeedPanel extends React.Component<{}, {}> {
     return (
       <div className="feed-panel">
         <div className="feed-items">
-          <h2>Feed Items</h2>
+        <h2>Feed Items</h2><button onClick = {() =>this.PopupRef.current?.Show()} className= "feed-button">Creeer feed item</button>
           {this.getFeedItems()}
+          
         </div>
-        <div className="feed-form">
-          <h2>Feed Item aanmaken</h2>
-          <form action="#" onSubmit={() => sendAsJSON({
-      title: this.InputRef.current?.value,
-      description: this.TextRef.current?.value,
-      category: this.CategorieRef.current?.value,
-    }, "/api/feedItem")}>
-            <input
-              type="text"
-              className="form-element"
-              placeholder="Titel"
-              id="title"
-              ref={this.InputRef}
-            ></input>
-            <br />
-            <textarea
-              className="form-element"
-              placeholder="Type uw bericht"
-              id="textfield"
-              ref={this.TextRef}
-            ></textarea>
-            <br />
-            {/* this is for the form  */}
-            <input
-              id="category"
-              type="text"
-              list="datalist"
-              placeholder="Selecteer een categorie"
-              className="form-element"
-              ref={this.CategorieRef}
-            />
-            <datalist id="datalist">
-              <option>Algemene mededeling</option>
-              <option>Notule</option>
-              <option>Persoonlijk</option>
-            </datalist>
+        
+        <PopUp ref = {this.PopupRef}>
+          <div className="feed-form">
+            <h2>Feed Item aanmaken</h2>
+            <form
+              action="#"
+              onSubmit={() =>
+                sendAsJSON(
+                  {
+                    title: this.InputRef.current?.value,
+                    description: this.TextRef.current?.value,
+                    category: this.CategorieRef.current?.value,
+                  },
+                  "/api/feedItem"
+                )
+              }
+            >
+              <input
+                type="text"
+                className="form-element"
+                placeholder="Titel"
+                id="title"
+                ref={this.InputRef}
+              ></input>
+              <br />
+              <textarea
+                className="form-element"
+                placeholder="Type uw bericht"
+                id="textfield"
+                ref={this.TextRef}
+              ></textarea>
+              <br />
+              {/* this is for the form  */}
+              <input
+                id="category"
+                type="text"
+                list="datalist"
+                placeholder="Selecteer een categorie"
+                className="form-element"
+                ref={this.CategorieRef}
+              />
+              <datalist id="datalist">
+                <option>Algemene mededeling</option>
+                <option>Notule</option>
+                <option>Persoonlijk</option>
+              </datalist>
 
-            <input type="submit" id="feed-form-submit" value="Aanmaken"></input>
-          </form>
-        </div>
+              <input
+                type="submit"
+                id="feed-form-submit"
+                value="Aanmaken"
+              ></input>
+            </form>
+          </div>
+        </PopUp>
       </div>
     );
   }
