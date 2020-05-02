e427cfffa5630000f2f38adc05168cca161a2e5e
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2020-05-02 22:46:39 +0200

    Validation voor de feed input

--- src/components/FeedPanel/FeedPanel.tsx
+++ src/components/FeedPanel/FeedPanel.tsx
@@ -32,16 +32,28 @@ export class FeedPanel extends React.Component<{}, {}> {
     return rows;
   }
 
-  sentFeedItem(){
-    this.PopupRef.current?.Hide()
-    sendAsJSON(
-      {
-        title: this.InputRef.current?.value,
-        description: this.TextRef.current?.value,
-        category: this.CategorieRef.current?.value,
-      },
-      "http://192.168.2.19:12002/api/feedItem"
-    )
+  //since inputs without form don't have validations this function does, if everthings alright it sends the feed
+  sentFeedItem() {
+    var inputs:NodeListOf<HTMLInputElement> | undefined = document.getElementsByClassName("feed-form").item(0)?.querySelectorAll("input[class = 'form-element']");
+    var filledIn: Boolean = true;
+    inputs?.forEach((x) =>
+      x.value === "" || x.value === "aanmaken"
+        ? (filledIn = false)
+        : true
+    );
+    if (filledIn === true) {
+      this.PopupRef.current?.Hide();
+      sendAsJSON(
+        {
+          title: this.InputRef.current?.value,
+          description: this.TextRef.current?.value,
+          category: this.CategorieRef.current?.value,
+        },
+        "http://192.168.2.19:12002/api/feedItem"
+      );
+    } else {
+      alert("Vul alle velden in alstublieft.");
+    }
   }
 
   render() {
@@ -61,44 +73,44 @@ export class FeedPanel extends React.Component<{}, {}> {
         <PopUp ref={this.PopupRef}>
           <div className="feed-form">
             <h2>Feed Item aanmaken</h2>
-              <input
-                type="text"
-                className="form-element"
-                placeholder="Titel"
-                id="title"
-                ref={this.InputRef}
-                required
-              ></input>
-              <br />
-              <textarea
-                className="form-element"
-                placeholder="Type uw bericht"
-                id="textfield"
-                ref={this.TextRef}
-                required
-              ></textarea>
-              <br />
-              {/* this is for the form  */}
-              <input
-                id="category"
-                type="text"
-                list="datalist"
-                placeholder="Selecteer een categorie"
-                className="form-element"
-                ref={this.CategorieRef}
-                required
-              />
-              <datalist id="datalist">
-                <option>Algemene mededeling</option>
-                <option>Notule</option>
-                <option>Persoonlijk</option>
-              </datalist>
+            <input
+              type="text"
+              className="form-element"
+              placeholder="Titel"
+              id="title"
+              ref={this.InputRef}
+              required
+            ></input>
+            <br />
+            <textarea
+              className="form-element"
+              placeholder="Type uw bericht"
+              id="textfield"
+              ref={this.TextRef}
+              required
+            ></textarea>
+            <br />
+            {/* this is for the form  */}
+            <input
+              id="category"
+              type="text"
+              list="datalist"
+              placeholder="Selecteer een categorie"
+              className="form-element"
+              ref={this.CategorieRef}
+              required
+            />
+            <datalist id="datalist">
+              <option>Algemene mededeling</option>
+              <option>Notule</option>
+              <option>Persoonlijk</option>
+            </datalist>
 
-              <input
-                onClick={() => this.sentFeedItem()}
-                type="button"
-                value="Aanmaken"/>
-
+            <input
+              onClick={() => this.sentFeedItem()}
+              type="button"
+              value="aanmaken"
+            />
           </div>
         </PopUp>
       </div>
